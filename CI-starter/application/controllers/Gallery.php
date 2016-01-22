c19d632eb871a2a27f2b7adf5450198d6fc5fdcf
evelyndai <daiyi68@live.cn>
2016-01-21 17:49:51 -0800

    Lab03-basic3 Complete

--- CI-starter/application/controllers/Gallery.php
+++ CI-starter/application/controllers/Gallery.php
@@ -28,13 +28,13 @@ class Gallery extends Application {
                 $cells[] = $this->parser->parse('_cell', (array) $picture, true);
             
             $this->load->library('table');
-            $parms - array(
+            $parms = array(
                 'table_open' => '<table class="gallery">',
                 'cell_start' => '<td class="oneimage">',
                 'cell_alt_start' => '<td class="oneimage">'
             );
             
-            $this->table->set_tmplate($parms);
+            $this->table->set_template($parms);
             
             $rows = $this->table->make_columns($cells, 3);
             $this->data['thetable'] = $this->table->generate($rows);
